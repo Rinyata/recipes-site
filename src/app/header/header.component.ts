ef1f1ca71571efb9e59fa9da4a08196e4341b87b
Rinyata <selinnsclk@gmail.com>
2023-08-04 11:16:11 +0300

    guncelleme 4

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  // searchBar!: HTMLElement;
+
+  // // Sayfa yüklendiğinde searchBar değişkenini alacak olan fonksiyon
+  // window.addEventListener('DOMContentLoaded', () => {
+  //   searchBar = document.getElementById('searchBar')!;
+  // });
+  
+  // // Fare search barın üzerine geldiğinde
+  // searchBar.addEventListener('mouseenter', () => {
+  //   searchBar.style.width = '200px'; // Genişliği 200 piksel yap
+  // });
+  
+  // // Fare search barın üzerinden çekildiğinde
+  // searchBar.addEventListener('mouseleave', () => {
+  //   searchBar.style.width = '150px'; // Genişliği başlangıç genişliğine (150px) döndür
+  // });
+  
+
+  
 }
