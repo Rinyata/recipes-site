b364aea419eac61a0e3004e3aea95e2f1c54be13
Rinyata <selinnsclk@gmail.com>
2023-08-07 17:49:34 +0300

    guncelleme 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   
+  { path: '', component: MainPageComponent },
   { path: 'app-main-page', component: MainPageComponent },
   { path: 'food/:foodId', component: FoodPageComponent },
   { path: 'app-left-menu', component: MainPageComponent}
